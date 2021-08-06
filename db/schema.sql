b8625fc622a1f8a0cb15cd972a00e284e3872a0f
unknown <jacobchrisb@gmail.com>
2021-08-05 20:49:41 -0400

    schema update

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,21 @@
-DROP DATABASE IF EXISTS company_db;
-CREATE database company_db;
+DROP DATABASE IF EXISTS 12.Employee-Trackerdb;
+CREATE database 12.Employee-Trackerdb;
+
+USE 12.Employee-Trackerdb;
+ 
+CREATE TABLE department (
+    dept_id INT NOT NULL AUTO_INCREMENT,
+    dept_name VARCHAR(30),
+    PRIMARY KEY (dept_id)
+);
+ 
+CREATE TABLE company_role (
+    role_id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DEC(7,2) NOT NULL,
+    dept_id INT,
+    PRIMARY KEY (role_id),
+    FOREIGN KEY (dept_id) REFERENCES department(dept_id)
+);
+ 
+CREATE TABLE employees (
